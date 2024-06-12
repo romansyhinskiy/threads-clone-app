50d5283b14c33107ec8811e2098eb0ef428eb78e
romansyhinskiy <romansyhinskiy@gmail.com>
2024-06-12 23:12:05 +0300

    add clerk auth redirect func

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
-// import { UserButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>Home</h2>
-      {/* <UserButton afterSignOutUrl="/" /> */}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
