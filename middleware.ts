bad461f342cbc59ad6e9976b86b1038bdee2e170
romansyhinskiy <romansyhinskiy@gmail.com>
2024-06-14 18:42:28 +0300

    Home page

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,15 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
+// Copy the middleware code as it is from the above resource
 
-export default clerkMiddleware();
+import { authMiddleware } from "@clerk/nextjs/server";
 
-// export default authMiddleware({
-//     publicRoutes: ['/', '/api/webhook/clerk'],
-//     ignoredRoutes: ['/api/webhook/clerk']
-// });
+export default authMiddleware({
+  // An array of public routes that don't require authentication.
+  publicRoutes: ["/api/webhook/clerk"],
+
+  // An array of routes to be ignored by the authentication middleware.
+  ignoredRoutes: ["/api/webhook/clerk"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
